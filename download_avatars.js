b0abf11acaff7db2df572f1240c6f403a6cec84f
JaffarShah <sjeffshah@gmail.com>
2019-04-09 16:02:18 -0400

    print avatar urls

--- download_avatars.js
+++ download_avatars.js
@@ -1,25 +1,31 @@
 var request = require('request');
-var secrets = require('Secret')
+var secrets = require('./secrets')
 function getRepoContributors(repoOwner, repoName, cb) {
     
   var options = {
       url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
       headers: {
-        'User-Agent': 'request'
-        'Authorization': "token " + somesecrettoken
+        'User-Agent': 'request',
+        'Authorization': "token " + secrets.GITHUB_TOKEN
       }
     };
-  
     request(options, function(err, res, body) {
-      cb(err, body);
+      if (res.statusCode === 200) {
+        var data = JSON.parse(body);
+        cb(null, data);
+      }
     });
   }
-  
+ 
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
     console.log("Errors:", err);
-    console.log("Result:", result);
+    for (let repo of result) {
+      console.log(repo.avatar_url)
+    }
+    // console.log("Result:", result);
 });
