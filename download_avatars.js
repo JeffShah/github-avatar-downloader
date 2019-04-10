1634d451784ecabd987f7ca21c2e0a4566348d53
JaffarShah <sjeffshah@gmail.com>
2019-04-09 22:19:47 -0400

    Added process.argv, Step 9

--- download_avatars.js
+++ download_avatars.js
@@ -1,37 +1,45 @@
 var request = require('request')
 var secrets = require('./secrets')
 var fs = require('fs')
+var args = process.argv.splice(2); // Step 9 cmd arguments
+
+console.log('Welcome to the GitHub Avatar Downloader!');
+//connects to github API
+
+if (args.length === 2){
 function getRepoContributors(repoOwner, repoName, cb) {
-    
-  var options = {
+    var options = {
       url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
       headers: {
         'User-Agent': 'request',
         'Authorization': "token " + secrets.GITHUB_TOKEN
       }
     };
+
+
     request(options, function(err, res, body) {
-      if (res.statusCode === 200) {
-        var data = JSON.parse(body);
-        cb(null, data);
-      }
-    });
+        cb(null, body);
+      });
+  }
+getRepoContributors(arg[0], arg[1], function(err, result, callback) {
+  console.log('Error:', err);
+  console.log('Download starts now');
+  var jaguar = JSON.parse(result);
+  for (var obj of jaguar) {
+    downloadImageByURL(obj['avatar_url'], './' + './avatars/' + obj['login'] + '.jpg', obj['login']);
   }
 
-function downloadImageByURL(url, filePath) {
-  request.get(url).pipe(fs.createWriteStream(filePath));
-  //console.log( "Downloading file ");  
+  function downloadImageByURL(url, filePath, login) {
+    request.get(url)
+    .on('error', function (err) {
+      console.log('There was an error: ', err);
+    })
+    .on('response', function (response) {
+      console.log('Image of: ' + login + ' downloaded!');
+    })
+    .pipe(fs.createWriteStream(filePath));
+  }
+  });
+} else {
+  console.log('GitHub Avatar Downloader depends on two (2) arguements. Please ensure both [username] [repo] are correct.');
 }
-
-
-console.log('Welcome to the GitHub Avatar Downloader!');
-
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  for (let repo of result) {
-  downloadImageByURL(repo.avatar_url, 'avatars/' + repo.login + '.jpg')
-
-  //console.log(repo.login)
-}
-     //console.log("Result:", result);
-});
